bceafa0e671731bb7d2b781c6d5a910e54a802e0
Luis Ochoa <ochoaal98@gmail.com>
2020-12-14 12:30:32 -0400

    feat(deps): Agregar dependencia TESLA HTTP CLIENT.

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule PullmanDashboard.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:tesla, "~> 1.4.0"},
+      {:hackney, "~> 1.16.0"}
     ]
   end
 
