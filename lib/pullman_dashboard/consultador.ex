9b5995a8c11cee3ba0b64060f08d259f9044d599
Luis Ochoa <ochoaal98@gmail.com>
2020-12-14 12:48:57 -0400

    feat: Método para obtener codigo de ciudad && documentación

--- lib/pullman_dashboard/consultador.ex
+++ lib/pullman_dashboard/consultador.ex
@@ -1,6 +1,11 @@
 defmodule PullmanDashboard.Consultador do
   require Logger
 
+  @doc """
+  Obtiene ciudades desde la API pública de Pullman y las devuelve
+  en un arreglo, en caso de error devuelve nil, o arroja excepción si es error
+  de decoding.
+  """
   def obtener_ciudades() do
     peticion_http =
       Tesla.post("https://pullman.cl/integrador-web/rest/private/venta/buscaCiudades", nil,
@@ -22,5 +27,19 @@ defmodule PullmanDashboard.Consultador do
             nil
         end
     end
+  end
+
+  @doc """
+  Obtiene el código de una ciudad mientras está se encuentre dentro de 
+  los datos devueltos por obtener_ciudades()
+
+  ## Ejemplo
+      iex> PullmanDashboard.Consultador.obtener_ciudades |> PullmanDashboard.Consultador.obtener_codigo_ciudad("CASTRO")
+      "10201298"
+  """
+  def obtener_codigo_ciudad(ciudades, ciudad) do
+    ciudades
+    |> Enum.find(%{}, fn c -> Map.get(c, "nombre") == ciudad end)
+    |> Map.get("codigo")
   end
 end
