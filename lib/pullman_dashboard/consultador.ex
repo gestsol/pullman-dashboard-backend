e0ce4c9a10ce981ae04f79e30710c187e2b2b332
Luis Ochoa <ochoaal98@gmail.com>
2020-12-14 12:32:50 -0400

    refactor: Ajusta indentation

--- lib/pullman_dashboard/consultador.ex
+++ lib/pullman_dashboard/consultador.ex
@@ -1,21 +1,26 @@
 defmodule PullmanDashboard.Consultador do
   require Logger
-  
+
   def obtener_ciudades() do
-  	peticion_http = Tesla.post("https://pullman.cl/integrador-web/rest/private/venta/buscaCiudades", nil, headers: [{"content-type", "application/json"}])
+    peticion_http =
+      Tesla.post("https://pullman.cl/integrador-web/rest/private/venta/buscaCiudades", nil,
+        headers: [{"content-type", "application/json"}]
+      )
 
     case peticion_http do
-      {:error, _} -> 
-      	nil
-      	Logger.error "Ocurrió error al obtenre ciudades"
+      {:error, _} ->
+        nil
+        Logger.error("Ocurrió error al obtenre ciudades")
+
       {:ok, response} ->
-      	case response.status do
-      	  200 ->
+        case response.status do
+          200 ->
             Jason.decode!(response.body)
-      	  _ -> 
-      	    Logger.error "Ocurrió error al obtener ciudades" 
-      	    nil
-      	end
-     end
+
+          _ ->
+            Logger.error("Ocurrió error al obtener ciudades")
+            nil
+        end
+    end
   end
 end
