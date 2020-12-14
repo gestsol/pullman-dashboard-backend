19e4544e8dc9f5647c112cc4acc4f28c9cf50c2d
Luis Ochoa <ochoaal98@gmail.com>
2020-12-14 12:31:12 -0400

    feat: Añade, modulo para consultas http y método para obtener listado de ciudades.

---
+++ lib/pullman_dashboard/consultador.ex
@@ -0,0 +1,21 @@
+defmodule PullmanDashboard.Consultador do
+  require Logger
+  
+  def obtener_ciudades() do
+  	peticion_http = Tesla.post("https://pullman.cl/integrador-web/rest/private/venta/buscaCiudades", nil, headers: [{"content-type", "application/json"}])
+
+    case peticion_http do
+      {:error, _} -> 
+      	nil
+      	Logger.error "Ocurrió error al obtenre ciudades"
+      {:ok, response} ->
+      	case response.status do
+      	  200 ->
+            Jason.decode!(response.body)
+      	  _ -> 
+      	    Logger.error "Ocurrió error al obtener ciudades" 
+      	    nil
+      	end
+     end
+  end
+end
