e852804bd5c14c4e980a0aea533f8222c4b03360
Luis Ochoa <ochoaal98@gmail.com>
2020-12-16 18:02:42 -0400

    feat(): Agrega archivo de ejemplo para referencia posterior.

---
+++ lib/pullman_dashboard/ejemplos/grilla_vertical.ex
@@ -0,0 +1,128 @@
+## 1 -> Primer piso
+## 2 -> Segundo piso
+
+%{
+  "1" => [
+    [
+      %{"asiento" => "B2", "estado" => "sinasiento"},
+      %{"asiento" => "B1", "estado" => "sinasiento"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "", "estado" => "sinasiento"},
+      %{"asiento" => "%", "estado" => "sinasiento"}
+    ],
+    [
+      %{"asiento" => "1", "estado" => "ocupado"},
+      %{"asiento" => "2", "estado" => "ocupado"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "", "estado" => "sinasiento"},
+      %{"asiento" => "3", "estado" => "ocupado"}
+    ],
+    [
+      %{"asiento" => "4", "estado" => "ocupado"},
+      %{"asiento" => "5", "estado" => "ocupado"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "", "estado" => "sinasiento"},
+      %{"asiento" => "6", "estado" => "ocupado"}
+    ],
+    [
+      %{"asiento" => "7", "estado" => "ocupado"},
+      %{"asiento" => "8", "estado" => "ocupado"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "", "estado" => "sinasiento"},
+      %{"asiento" => "9", "estado" => "ocupado"}
+    ],
+    [
+      %{"asiento" => "10", "estado" => "ocupado"},
+      %{"asiento" => "11", "estado" => "ocupado"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "", "estado" => "sinasiento"},
+      %{"asiento" => "12", "estado" => "ocupado"}
+    ]
+  ],
+  "2" => [
+    [
+      %{"asiento" => "21", "estado" => "ocupado"},
+      %{"asiento" => "22", "estado" => "libre"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "23", "estado" => "libre"},
+      %{"asiento" => "24", "estado" => "ocupado"}
+    ],
+    [
+      %{"asiento" => "25", "estado" => "ocupado"},
+      %{"asiento" => "26", "estado" => "ocupado"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "%", "estado" => "sinasiento"},
+      %{"asiento" => "%", "estado" => "sinasiento"}
+    ],
+    [
+      %{"asiento" => "27", "estado" => "libre"},
+      %{"asiento" => "28", "estado" => "libre"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "%", "estado" => "sinasiento"},
+      %{"asiento" => "%", "estado" => "sinasiento"}
+    ],
+    [
+      %{"asiento" => "29", "estado" => "ocupado"},
+      %{"asiento" => "30", "estado" => "libre"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "31", "estado" => "ocupado"},
+      %{"asiento" => "32", "estado" => "ocupado"}
+    ],
+    [
+      %{"asiento" => "33", "estado" => "libre"},
+      %{"asiento" => "34", "estado" => "ocupado"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "35", "estado" => "ocupado"},
+      %{"asiento" => "36", "estado" => "ocupado"}
+    ],
+    [
+      %{"asiento" => "37", "estado" => "ocupado"},
+      %{"asiento" => "38", "estado" => "ocupado"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "39", "estado" => "ocupado"},
+      %{"asiento" => "40", "estado" => "ocupado"}
+    ],
+    [
+      %{"asiento" => "41", "estado" => "libre"},
+      %{"asiento" => "42", "estado" => "ocupado"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "43", "estado" => "libre"},
+      %{"asiento" => "44", "estado" => "libre"}
+    ],
+    [
+      %{"asiento" => "45", "estado" => "libre"},
+      %{"asiento" => "46", "estado" => "libre"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "47", "estado" => "ocupado"},
+      %{"asiento" => "48", "estado" => "libre"}
+    ],
+    [
+      %{"asiento" => "49", "estado" => "ocupado"},
+      %{"asiento" => "50", "estado" => "ocupado"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "51", "estado" => "ocupado"},
+      %{"asiento" => "52", "estado" => "libre"}
+    ],
+    [
+      %{"asiento" => "53", "estado" => "libre"},
+      %{"asiento" => "54", "estado" => "libre"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "55", "estado" => "libre"},
+      %{"asiento" => "56", "estado" => "libre"}
+    ],
+    [
+      %{"asiento" => "57", "estado" => "libre"},
+      %{"asiento" => "58", "estado" => "libre"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "59", "estado" => "libre"},
+      %{"asiento" => "60", "estado" => "libre"}
+    ],
+    [
+      %{"asiento" => "61", "estado" => "libre"},
+      %{"asiento" => "62", "estado" => "libre"},
+      %{"asiento" => "", "estado" => "pasillo"},
+      %{"asiento" => "63", "estado" => "libre"},
+      %{"asiento" => "64", "estado" => "libre"}
+    ]
+  ]
+}
